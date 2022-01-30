dc9e8e1be094c4f2995e9880632f505ce1bc2854
Kaifeng Yu <kaifeng.yu625@outlook.com>
2022-01-30 03:54:34 +0000

    add test

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from transformers import pipeline, AutoModelWithLMHead, AutoTokenizer
-model = AutoModelWithLMHead.from_pretrained("Helsinki-NLP/opus-mt-zh-en")
-tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-zh-en")
-translation = pipeline("translation_en_to_zh", model=model, tokenizer=tokenizer)
 
-text = "一个人"
-translated_text = translation(text, max_length=40)[0]['translation_text']
-print(translated_text)
+def translate(text):
+    model = AutoModelWithLMHead.from_pretrained("Helsinki-NLP/opus-mt-zh-en")
+    tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-zh-en")
+    translation = pipeline("translation_en_to_zh", model=model, tokenizer=tokenizer)
+    translated_text = translation(text, max_length=40)[0]['translation_text']
+    return translated_text
---
+++ test_main.py
@@ -0,0 +1,5 @@
+from main import translate
+
+def test_translate():
+    text = "一个人"
+    printf(translate(text))
