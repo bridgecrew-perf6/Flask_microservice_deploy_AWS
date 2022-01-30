57db6232f0da4c26a01fd766e87ced0cec9e0a6d
kaifeng-yu16 <90477174+kaifeng-yu16@users.noreply.github.com>
2022-01-30 00:08:35 -0500

    Delete .~c9_invoke_1McNu.py

--- .~c9_invoke_1McNu.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline, AutoModelWithLMHead, AutoTokenizer
-model = AutoModelWithLMHead.from_pretrained("Helsinki-NLP/opus-mt-en-zh")
-tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-zh")
-translation = pipeline("translation_en_to_zh", model=model, tokenizer=tokenizer)
-
-text = "一个人"
-translated_text = translation(text, max_length=40)[0]['translation_text']
-print(translated_text)
