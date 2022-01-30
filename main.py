51eb6f88f9c587e498ae212bc3c1ae1f8ba545af
Kaifeng Yu <kaifeng.yu625@outlook.com>
2022-01-30 04:02:30 +0000

    add test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from transformers import pipeline, AutoModelWithLMHead, AutoTokenizer
+from transformers import pipeline, AutoModelWithLMHead, 
+from flask import Flask
 
 def translate(text):
     model = AutoModelWithLMHead.from_pretrained("Helsinki-NLP/opus-mt-zh-en")
@@ -6,3 +7,11 @@ def translate(text):
     translation = pipeline("translation_en_to_zh", model=model, tokenizer=tokenizer)
     translated_text = translation(text, max_length=40)[0]['translation_text']
     return translated_text
+    
+@app.get("/")
+async def do_translate():
+    
+    
+    
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
