d2f366d575fdc14735ae2101fdb6ac3cf6614158
kaifeng-yu16 <90477174+kaifeng-yu16@users.noreply.github.com>
2022-03-19 05:33:20 -0400

    Create download_model.py

---
+++ download_model.py
@@ -0,0 +1,4 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-zh-en")
+model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-zh-en")
