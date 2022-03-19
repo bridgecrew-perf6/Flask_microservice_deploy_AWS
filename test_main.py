a411049cf07039a971a6529bb51c90c68a11ca59
kaifeng-yu16 <90477174+kaifeng-yu16@users.noreply.github.com>
2022-03-19 05:30:56 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,3 +3,11 @@ from main import translate
 def test_translate():
     text = "一个人"
     assert (translate(text) == 'Alone.')
+    
+def test_home():
+    response = app.test_client().get("/")
+    assert response.status_code == 200
+
+def test_translate():
+    response = app.test_client().get("/translate")
+    assert response.status_code == 200
